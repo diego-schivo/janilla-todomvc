3bd117f0f5feac309fdd3f88a251b1f53f3b6695
Diego Schivo <diego.schivo@gmail.com>
2025-11-23 07:23:40 +0100

    Converter

--- main/source/com/janilla/todomvc/TodoMvc.java
+++ main/source/com/janilla/todomvc/TodoMvc.java
@@ -41,12 +41,12 @@ import javax.net.ssl.SSLContext;
 import com.janilla.http.HttpHandler;
 import com.janilla.http.HttpServer;
 import com.janilla.ioc.DiFactory;
+import com.janilla.java.DollarTypeResolver;
 import com.janilla.java.Java;
-import com.janilla.json.DollarTypeResolver;
-import com.janilla.json.TypeResolver;
+import com.janilla.java.TypeResolver;
 import com.janilla.net.Net;
-import com.janilla.reflect.ClassAndMethod;
 import com.janilla.web.ApplicationHandlerFactory;
+import com.janilla.web.Invocable;
 import com.janilla.web.Handle;
 import com.janilla.web.NotFoundException;
 import com.janilla.web.Render;
@@ -100,7 +100,7 @@ public class TodoMvc {
 		{
 			var f = diFactory.create(ApplicationHandlerFactory.class, Map.of("methods", types().stream()
 					.flatMap(x -> Arrays.stream(x.getMethods()).filter(y -> !Modifier.isStatic(y.getModifiers()))
-							.map(y -> new ClassAndMethod(x, y)))
+							.map(y -> new Invocable(x, y)))
 					.toList(), "files",
 					Stream.of("com.janilla.frontend", TodoMvc.class.getPackageName())
 							.flatMap(x -> Java.getPackagePaths(x).stream().filter(Files::isRegularFile)).toList()));
