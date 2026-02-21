6ba30328a4b4c20d3daac43722f0d64523e502cb
Diego Schivo <diego.schivo@gmail.com>
2026-02-21 06:40:04 +0100

    generateKeyPair

--- test/source/com/janilla/todomvc/test/TodoMvcTest.java
+++ test/source/com/janilla/todomvc/test/TodoMvcTest.java
@@ -83,7 +83,7 @@ public class TodoMvcTest {
 					p = System.getProperty("user.home") + p.substring(1);
 				var f = Path.of(p);
 				if (!Files.exists(f))
-					Java.generateKeyPair(f, w);
+					Java.generateKeyPair("localhost", f, w, "dns:localhost,ip:127.0.0.1");
 				try (var s = Files.newInputStream(f)) {
 					c = Java.sslContext(s, w.toCharArray());
 				} catch (IOException e) {
